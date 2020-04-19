ef24477cf23dce1f9c14045c111bd71de5c75f42
Ricard <r.molina91@gmail.com>
2020-04-19 12:51:31 +0200

    Fix exceptions for stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -11,6 +11,19 @@ module.exports = {
     "no-eol-whitespace": null,
     "declaration-empty-line-before": null,
     "value-keyword-case": null,
+    "at-rule-no-unknown": [
+      true,
+      {
+        "ignoreAtRules": [
+          "tailwind",
+          "apply",
+          "responsive",
+          "variants",
+          "screen",
+          "use"
+        ]
+      }
+    ],
     "plugin/selector-bem-pattern": {
       "componentName": "[A-Z]+",
       "componentSelectors": {
